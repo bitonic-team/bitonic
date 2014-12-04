6a8f2c377f08974d278c1a102c7eb3d27afc350a
DavoCg <david.cingala@gmail.com>
2014-12-04 16:59:15 +0100

    second commit

--- server/server.js
+++ server/server.js
@@ -1,50 +1,7 @@
 var http = require("http");
-var mysql = require("mysql");
-
-var nconf = require('nconf');
-var fs = require('fs');
-var config_file = fs.existsSync('config.json') ? 'config.json' : 'config-local.json';
-var configs = nconf.file({ file: config_file });
-
-var connection = mysql.createConnection({
-  host     : nconf.get('mysql_host'),
-  user     : nconf.get('mysql_user'),
-  password : nconf.get('mysql_password'),
-});
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://mongo');
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback () {
-  console.log('mongoose worked fine !!!!')
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-
-  console.log('connected as id ' + connection.threadId);
-});
-
-
 
 var server = http.createServer(function(request, response) {
-
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("<!DOCTYPE html>");
-  response.write("<html>");
-  response.write("<head>");
-  response.write("<title>Hello World Page 1</title>");
-  response.write("</head>");
-  response.write("<body>");
-  response.write("Hello World! 2");
-  response.write("</body>");
-  response.write("</html>");
-  response.end();
+  response.end('hello world');
 });
  
 server.listen(80);
