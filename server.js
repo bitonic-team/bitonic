173216673e76cd287fcd4ec2a84489c33a106dd3
lighta971 <lighta971@live.fr>
2014-12-04 22:51:03 +0100

    fix server route

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ var mongoose   = require('mongoose');
 var configDB  = require('./server/config/database.js');
 var port       = process.env.PORT || 80;
 
-var apiRoute = require('./server/routes/api')(app);
 
 // CONNECT TO DB
 mongoose.connect(configDB.url);
 
 app.use(express.static(__dirname + '/public'));
-app.use('/api', apiRoute);
+require('./server/routes/api')(app);
+// app.use('/api', apiRoute);
 
 app.get('/*', function (req, res) {
   res.redirect("/public/index.html");
